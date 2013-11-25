c72c8111f7afdbafa71523c5b3a780f839c5990a
Colin Mitchell <muffinista@gmail.com>
2013-11-24 20:32:19 -0500

    assorted updates

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -29,3 +29,16 @@
 (setq load-path (append (list (expand-file-name "~/.emacs.d/libs/js2-mode")) load-path))
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-enabled-themes (quote (manoj-dark))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- home/.zshrc
+++ home/.zshrc
@@ -7,7 +7,8 @@ export ZSH=$HOME/.oh-my-zsh
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
 #ZSH_THEME="robbyrussell"
-ZSH_THEME="cypher"
+#ZSH_THEME="cypher"
+ZSH_THEME="superjarin"
 
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
@@ -33,7 +34,7 @@ source "$HOME/.rvm/scripts/rvm"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(rails3 ruby git command-not-found rvm bundler)
+plugins=(rails3 ruby git command-not-found rvm bundler gitfast)
 
 source $HOME/.zsh-colin
 source $HOME/.zsh-private
